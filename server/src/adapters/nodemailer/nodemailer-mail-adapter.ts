acbe51cacab6a969ef36b654191bcecaed0e64b8
Diego Alves <diegoalvesdc12@gmail.com>
2022-05-09 13:13:26 -0300

    First Commit

---
+++ server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import nodemailer from 'nodemailer';
+import { MailAdapter, SendMailData } from "../mail-adapter";
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "7c6366a8574ebe",
+    pass: "a9af3110c06902"
+  }
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: 'Equipe Feedget <oi@feedget.com>',
+      to: 'Diego Alves <diegoalvesdc12@gmail.com>',
+      subject,
+      html: body
+    })
+  }
+}
